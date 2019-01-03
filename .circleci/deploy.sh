5376820b4bdac872b939270ebbcab57b216fbaaa
Jake Howard <git@theorangeone.net>
2019-01-03 13:38:59 +0000

    Add HEAD commit SHA instead

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,4 +8,4 @@ git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-pipenv run mkdocs gh-deploy --verbose -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]\n$CIRCLE_COMPARE_URL"
+pipenv run mkdocs gh-deploy --verbose -m "Deploy build $CIRCLE_BUILD_NUM - $CIRCLECI_SHA1 [ci skip]"
